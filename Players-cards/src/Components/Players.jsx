fbf3bd5ef57e73dad399960975ef0d9d271758d6
unknown <kelvinuzo37@gmail.com>
2023-12-05 14:14:02 +0100

    React-props checkpoint

---
+++ Players-cards/src/Components/Players.jsx
@@ -0,0 +1,37 @@
+const players = [
+    {
+      name: "Cristiano Ronaldo",
+      team: "Al Nasir",
+      nationality: "Portugal",
+      jerseyNumber: 7,
+      age: 36,
+      imageUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTT2v0dWrBFNjcarJM0ljeZeBcCON3OUWfR7bo93AJIWJiG4qCqdP3dRq7O3-SF-hRK0M4&usqp=CAU",
+    },
+    {
+        name: "Kylian Mbappe",
+        team: "PSG",
+        nationality: "France",
+        jerseyNumber: 7,
+        age: 24,
+        imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxCO4S3zEsLhVsCv5aFOQpczQAPmxTYr0hE6bymCffPkunsniTgsSb_7GddktUXGkX6to&usqp=CAU",
+    },
+    {
+        name: "Lionel Messi",
+        team: "inter miami",
+        nationality: "Argentina",
+        jerseyNumber: 10,
+        age: 34,
+        imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHOlr3wFB8PRErFQlecpClAXWUkTVJecaiaA&usqp=CAU",
+      },
+      {
+        name: "Mohamed Salah",
+        team: "Liverpool",
+        nationality: "Egypt",
+        jerseyNumber: 11,
+        age: 31,
+        imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTGM3MtdJ5Aw2RHGNB6ohtJxPUunBxGCXen5g&usqp=CAU",
+      },
+    // Add more players as needed
+  ];
+  
+  export default players;
